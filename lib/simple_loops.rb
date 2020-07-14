53f0a86f348e99f4214319f389feb8577de69b05
Jarrell Jackson <jdjack3@gmail.com>
2020-07-14 16:32:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,6 @@
 # Write your methods here
-loop_message_five_times = 0
+def loop_message_five_times(message) 
+  loop_message_five_times = 0
 while loop_message_five_times < 5
   puts message
   counter += 1
